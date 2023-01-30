3e371ac55f37a9bf206c8abab4323bcba871f31b
DanSanabria <danielsanabria12396@gmail.com>
2023-01-30 11:31:10 -0500

    cambios

--- logica_programacion_1.js
+++ logica_programacion_1.js
@@ -11,10 +11,13 @@ numero2=parseInt(numero2);
 numero3=parseInt(numero3);
 
 if (numero1>numero2 && numero2>numero3) {
+    console.log(numero3,numero2,numero1)
     console.log(numero1,numero2,numero3)
 } else if(numero2>numero1 && numero1>numero3){
+    console.log(numero3,numero1,numero2)
     console.log(numero2,numero1,numero3)
 } else(numero3>numero1 && numero1>numero2)
+console.log(numero2,numero1,numero3)
 console.log(numero3,numero1,numero2)
 
 
